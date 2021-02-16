6f47342847e118282de89669753192401bb47779
Rodolfo Berlezi <rodolfo_berlezi@hotmail.com>
2021-02-16 17:59:48 -0300

    several improviments

--- javascript/animations.js
+++ javascript/animations.js
@@ -2,7 +2,7 @@ var roundLogEl = document.querySelector('.round');
 
 anime({
   targets: roundLogEl,
-  innerHTML: [0, 30],
+  innerHTML: [0, 50],
   easing: 'linear',
   round: 1 // Will round the animated value to 1 decimal
 });
